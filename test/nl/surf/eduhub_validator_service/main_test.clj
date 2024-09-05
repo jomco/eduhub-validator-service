4d300e538800607f4e699f6bff732105ec808a68
Michiel de Mare <merloen@gmail.com>
2024-09-05 16:44:26 +0200

    Added tests (#2)
    * Added tests
    
    * Review feedback

---
+++ test/nl/surf/eduhub_validator_service/main_test.clj
@@ -0,0 +1,14 @@
+(ns nl.surf.eduhub-validator-service.main-test
+  (:require [babashka.http-client :as http]
+            [clojure.test :refer :all]
+            [nl.surf.eduhub-validator-service.main :as main]))
+
+(deftest test-validate-correct
+  (with-redefs [http/request (fn [_] {:status 200 :body "mocked response"})]
+    (is (= {:status 200 :body "Endpoint validation successful" :headers {}}
+           (main/app-routes {:uri "/endpoints/google.com/config" :request-method :get})))))
+
+(deftest test-validate-fails
+  (with-redefs [http/request (fn [_] {:status 401 :body "mocked response"})]
+    (is (= {:status 502 :body "Endpoint validation failed with status: 401" :headers {}}
+           (main/app-routes {:uri "/endpoints/google.com/config" :request-method :get})))))
