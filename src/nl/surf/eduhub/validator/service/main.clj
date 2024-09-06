a16fe6c2de8527031aadda0a12c95d64835b7627
Michiel de Mare <michieldemare@gmail.com>
2024-09-06 13:08:25 +0200

    Added configuration

--- src/nl/surf/eduhub/validator/service/main.clj
+++ src/nl/surf/eduhub/validator/service/main.clj
@@ -1,48 +1,61 @@
 (ns nl.surf.eduhub.validator.service.main
   (:gen-class)
   (:require [babashka.http-client :as http]
-            [clojure.string :as string]
             [compojure.core :refer [defroutes GET]]
             [compojure.route :as route]
             [clojure.tools.logging :as log]
+            [environ.core :refer [env]]
+            [nl.jomco.envopts :as envopts]
             [nl.jomco.http-status-codes :as http-status]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [ring.middleware.json :refer [wrap-json-response]]))
 
-(def gateway-url (System/getenv "GATEWAY_URL"))
-(def gateway-auth
-  (let [[user pass] (string/split (System/getenv "GATEWAY_BASIC_AUTH") #":")]
-    {:user user :pass pass}))
-
-(defn validate-endpoint [endpoint-id]
+(defn validate-endpoint [endpoint-id config]
   (log/info "validating endpoint: " endpoint-id)
   (try
-    (let [response (http/get (str gateway-url "courses")
+    (let [response (http/get (str (:gateway-url config) "courses")
                              {:headers {"x-route" (str "endpoint=" endpoint-id)
                                         "accept" "application/json; version=5"
                                         "x-envelope-response" "false"}
-                              :basic-auth gateway-auth})]
+                              :basic-auth (:gateway-basic-auth config)})]
       (if (= (:status response) 200)
-        {:valid true :message "Endpoint validation successful"}
+        {:valid true}
         {:valid false :message (str "Endpoint validation failed with status: " (:status response))}))
     (catch Throwable e
       {:valid false :message (str "Error during validation: " (.getMessage e))})))
 
 (defroutes app-routes
   (GET "/endpoints/:endpoint-id/config" [endpoint-id]
-    (fn [_]
-      (let [{:keys [message valid]} (validate-endpoint endpoint-id)]
-        {:body message
-         :status (if valid
-                   http-status/ok
-                   http-status/bad-gateway)})))
+    (fn [_] {:validator true :endpoint-id endpoint-id}))
   (route/not-found "Not Found"))
 
-(def app
-  (-> app-routes
-      (wrap-defaults api-defaults)
-      wrap-json-response))
+(defn wrap-validator [app config]
+  (fn [req]
+    (let [{:keys [validator endpoint-id] :as resp} (app req)]
+      (if validator
+        (let [{:keys [valid] :as body} (validate-endpoint endpoint-id config)]
+          {:body body
+           :status (if valid
+                     http-status/ok
+                     http-status/bad-gateway)})
+        resp))))
+
+(def opt-specs
+  {:gateway-url             ["URL of gateway" :str :in [:gateway-url]]
+   :gateway-basic-auth-user ["Basic auth username of gateway" :str :in [:gateway-basic-auth :user]]
+   :gateway-basic-auth-pass ["Basic auth password of gateway" :str :in [:gateway-basic-auth :pass]]})
 
 (defn -main [& _]
-  (run-jetty app {:port 3000}))
+  (let [[config errs] (envopts/opts env opt-specs)]
+    (prn config)
+    (when errs
+      (.println *err* "Error in environment configuration")
+      (.println *err* (envopts/errs-description errs))
+      (.println *err* "Available environment vars:")
+      (.println *err* (envopts/specs-description opt-specs))
+      (System/exit 1))
+    (run-jetty (-> app-routes
+                   (wrap-validator config)
+                   (wrap-defaults api-defaults)
+                   wrap-json-response) {:port 3000})))
--- test/nl/surf/eduhub/validator/service/main_test.clj
+++ test/nl/surf/eduhub/validator/service/main_test.clj
@@ -1,14 +1,18 @@
 (ns nl.surf.eduhub.validator.service.main-test
   (:require [babashka.http-client :as http]
-            [clojure.test :refer :all]
+            [clojure.test :refer [deftest is]]
             [nl.surf.eduhub.validator.service.main :as main]))
+
+(def app (main/wrap-validator main/app-routes {}))
 
 (deftest test-validate-correct
   (with-redefs [http/request (fn [_] {:status 200 :body "mocked response"})]
-    (is (= {:status 200 :body "Endpoint validation successful" :headers {}}
-           (main/app-routes {:uri "/endpoints/google.com/config" :request-method :get})))))
+    (is (= {:status 200
+            :body {:valid true} }
+           (app {:uri "/endpoints/google.com/config" :request-method :get})))))
 
 (deftest test-validate-fails
   (with-redefs [http/request (fn [_] {:status 401 :body "mocked response"})]
-    (is (= {:status 502 :body "Endpoint validation failed with status: 401" :headers {}}
-           (main/app-routes {:uri "/endpoints/google.com/config" :request-method :get})))))
+    (is (= {:status 502
+            :body {:message "Endpoint validation failed with status: 401", :valid false}}
+           (app {:uri "/endpoints/google.com/config" :request-method :get})))))
